b63d779e9b9b0aa8cdde167a2693a64b24dfc876
BrainyHaron <Grow-f40@narod.ru>
2020-05-03 14:53:11 +0500

    Answer for Lesson 5

--- devisor_master.py
+++ devisor_master.py
@@ -16,8 +16,6 @@ def prime_num_check(n):
 prime_num_check(n)
 
 
-
-
 '''Task 2'''
 def deviders (n):
     print("Все делители целого числа:", end=" ")
@@ -26,3 +24,35 @@ def deviders (n):
             print(i, end=" ")
 deviders(n)
 
+
+'''Task 3'''
+
+def min_max_divider (n):
+    min_divider = n
+    max_divider = 1
+    for i in range(n - 1, 1, -1):
+        if (n % i == 0):
+            if (min_divider > i):
+                min_divider = i
+            if (max_divider < i):
+                max_divider = i
+    print("Минимальный делитель, кроме ""1"" равен:", min_divider)
+    print("Максимальный делитель, кроме", n, "равен:", max_divider)
+min_max_divider(n)
+
+
+'''Task 4 (Pro, Light+)'''
+
+# numb = int(input("Введите целое число: "))
+
+def only_prime_dividers (n):
+    print("Только простые делители:", end=" ")
+    for i in range(n - 1, 1, -1):
+        is_simple = 0
+        if (n % i == 0):
+            for j in range(i - 1, 1, -1):
+                if (i % j == 0):
+                    is_simple = is_simple + 1  # Увеличиваем, если находим делитель
+            if (is_simple == 0):  # Если делителей не было найдено, выводим
+                print(i, end=" ")
+only_prime_dividers(n)
